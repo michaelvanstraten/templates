13a39ff2d1ba1464bbb9ca78ab7c93d0f592429a
Michael van Straten <michael@vanstraten.de>
2025-10-05 16:34:51 +0200

    Add GitHub workflow to run template checks

--- checks/default.nix
+++ checks/default.nix
@@ -4,38 +4,7 @@
   pre-commit-hooks,
   pkgs,
 }:
-
-let
-  # Function to create a check for a template by running `nix flake check`
-  checkTemplate =
-    name: template:
-    let
-      checkName = "${name}-template-check";
-    in
-    {
-      name = checkName;
-      value =
-        pkgs.runCommand checkName
-          {
-            buildInputs = with pkgs; [
-              nix
-              cacert
-            ];
-          }
-          ''
-            # Use `--print-build-logs` for verbose output during CI runs
-            nix flake check --print-build-logs ${template.path} > $out 2>&1
-          '';
-    };
-
-  # Generate a list of checks for all templates in `self.templates`
-  templateChecks = pkgs.lib.attrsets.mapAttrs' (
-    name: template: checkTemplate name template
-  ) self.templates;
-
-in
-templateChecks
-// {
+{
   # Pre-commit check configuration using hooks
   pre-commit-check = pre-commit-hooks.lib.${system}.run {
     src = ./.;
